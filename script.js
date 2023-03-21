ae293b14cf184f28d90acdf48d290a055d4a1851
Omkar Kirpan <okirpan@gmail.com>
2023-03-21 23:54:53 +0530

    fix: sanitize input

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+function sanitizeInput(input) {
+  // regular expression to match any characters that are not digits, operators, or decimal points
+  const regex = /[^0-9+\-*/.]/g;
+  // replace any matches with an empty string
+  return input.replace(regex, "");
+}
+
 let string = "";
 let buttons = document.querySelectorAll(".button");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
+      // sanitize the input
+      const sanitizedInput = sanitizeInput(string);
+      // evaluate the sanitized input
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML == "C") {
       string = "";
@@ -14,6 +23,11 @@ Array.from(buttons).forEach((button) => {
       document.querySelector("input").value = string;
     }
   });
+});
+
+let input = document.querySelector(".input");
+input.addEventListener("change", (e) => {
+  e.target.value = sanitizeInput(e.target.value);
 });
 
 document.addEventListener("keydown", (e) => {
