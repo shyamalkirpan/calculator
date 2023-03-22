1a60f4412b2e4851803665a92be210f8a30e7b83
Omkar Kirpan <okirpan@gmail.com>
2023-03-22 21:25:45 +0530

    fix: add readonly to display

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function sanitizeInput(input) {
   // regular expression to match any characters that are not digits, operators, or decimal points
   const regex = /[^0-9+\-*/.]/g;
-  // replace any matches with an empty string
+  // replace any matches with an empty istring
   return input.replace(regex, "");
 }
 
-let string = "";
+let inputString = "";
 let buttons = document.querySelectorAll(".button");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
       // sanitize the input
-      const sanitizedInput = sanitizeInput(string);
+      const sanitizedInput = sanitizeInput(inputString);
       // evaluate the sanitized input
-      document.querySelector("input").value = string;
+      document.querySelector("input").value = eval(sanitizedInput);
     } else if (e.target.innerHTML == "C") {
-      string = "";
-      document.querySelector("input").value = string;
+      inputString = "";
+      document.querySelector("input").value = inputString;
     } else {
       console.log(e.targer);
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
+      inputString = inputString + e.target.innerHTML;
+      document.querySelector("input").value = inputString;
     }
   });
 });
 
-let input = document.querySelector(".input");
-input.addEventListener("change", (e) => {
-  e.target.value = sanitizeInput(e.target.value);
-});
+// let input = document.querySelector(".input");
+// input.addEventListener("change", (e) => {
+//   e.target.value = sanitizeInput(e.target.value);
+// });
 
 document.addEventListener("keydown", (e) => {
   let key = e.key;
